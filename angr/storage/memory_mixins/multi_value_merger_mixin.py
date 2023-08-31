66bb5a92e3f7e488f6ceddd22e1967f448261e9e
Fish <ltfish@users.noreply.github.com>
2023-08-31 11:11:54 -0400

    MultiValueMergerMixin: Limit the number of annotations and avoid OOM. (#4117)

--- angr/storage/memory_mixins/multi_value_merger_mixin.py
+++ angr/storage/memory_mixins/multi_value_merger_mixin.py
@@ -4,8 +4,9 @@ from . import MemoryMixin
 
 
 class MultiValueMergerMixin(MemoryMixin):
-    def __init__(self, *args, element_limit=5, top_func=None, phi_maker=None, **kwargs):
+    def __init__(self, *args, element_limit=5, annotation_limit=40, top_func=None, phi_maker=None, **kwargs):
         self._element_limit = element_limit
+        self._annotation_limit = annotation_limit
         self._top_func: Callable = top_func
         self._phi_maker: Optional[Callable] = phi_maker
 
@@ -23,10 +24,14 @@ class MultiValueMergerMixin(MemoryMixin):
             top_val = self._top_func(merged_size * self.state.arch.byte_width)
             # migrate annotations
             annotations = []
+            annotations_set = set()
             for v in values_set:
-                annotations += list(v.annotations)
+                for anno in v.annotations:
+                    if anno not in annotations_set:
+                        annotations.append(anno)
+                        annotations_set.add(anno)
             if annotations:
-                top_val = top_val.annotate(*annotations)
+                top_val = top_val.annotate(*annotations[: self._annotation_limit])
             merged_val = {top_val}
         else:
             merged_val = values_set
@@ -35,6 +40,7 @@ class MultiValueMergerMixin(MemoryMixin):
     def copy(self, memo=None):
         copied = super().copy(memo)
         copied._element_limit = self._element_limit
+        copied._annotation_limit = self._annotation_limit
         copied._top_func = self._top_func
         copied._phi_maker = self._phi_maker
         return copied
