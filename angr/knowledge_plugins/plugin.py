6a4612c3412f3d3661af261c47b2efafaada6771
Florian Magin <fmagin@users.noreply.github.com>
2024-02-28 08:59:48 +0100

    Experimental KnowledgeViewPlugin

--- angr/knowledge_plugins/plugin.py
+++ angr/knowledge_plugins/plugin.py
@@ -21,3 +21,22 @@ class KnowledgeBasePlugin:
         if name in default_plugins:
             raise Exception(f"{default_plugins[name]} is already set as the default for {name}")
         default_plugins[name] = cls
+
+
+class KnowledgeViewPlugin(KnowledgeBasePlugin):
+    """
+    This represents a special kind of knowledge that is "frozen" after initial generation
+    Some KB Plugins function like databases/datastores, where any analysis can e.g. add xrefs in addition to just
+    looking up existing xrefs
+
+    but for some kinds of knowledge there is a clear "initial generation" phase, after which the knowledge never changes
+    e.g. if metadata is parsed from a binary, it is not expected to change after the initial extraction
+
+    This gathering of data shouldn't happen inside the plugin, but be a separate analysis
+    This separate analysis can then also internally use KnowledgeBase.request_knowledge on some other KnowledgeViewPlugin
+    to get some data that is required for the analysis
+
+    """
+
+    def gather(self):
+        raise NotImplementedError()
